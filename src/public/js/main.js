79b62ee354f1b166d0ec2fc63e0582c5d3321887
abduvohid <74610688+abduvohid20051013@users.noreply.github.com>
2022-01-08 11:05:18 +0500

    Add files via upload

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,25 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navList = document.querySelector(".nav-list");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+    if(!menuOpen) {
+        menuBtn.classList.add("open");
+        navList.classList.add("nav-open");
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove("open");
+        navList.classList.remove("nav-open");
+        menuOpen = false;
+    }
+})
 
+const galeryLinkOpen = document.querySelector(".more-galery__link");
+
+galeryLinkOpen.addEventListener("click", function(e) {
+    e.preventDefault();
+    const hiddenList = document.querySelector(".hidden-gallery__list");
+    const hiddenList2 = document.querySelector(".hidden-gallery__list-2");
+    hiddenList.classList.add("appear-list");
+    hiddenList2.classList.add("appear-list");
+    this.classList.add("none");
+})
---
+++ src/public/js/query.js
@@ -0,0 +1,23 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop()) {
+        $('#toTop').fadeIn();
+    } else {
+        $('#toTop').fadeOut();
+    }
+});
+
+$("#toTop").click(function () {
+    $("html, body").animate({scrollTop: 0}, 1000);
+ });
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+    $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 4000);   
+
